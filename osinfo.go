42e16db07870ff0f1bd1971bbc141cc6ec70a189
Diep Pham <me@favadi.com>
2016-10-07 12:31:14 +0700

    adds Release helper

--- osinfo.go
+++ osinfo.go
@@ -2,6 +2,11 @@ package osinfo
 
 import "fmt"
 
+const (
+	// UnknownRelease returns when can't determinate os release.
+	UnknownRelease = "unknown"
+)
+
 // OSInfo contains information of running OS.
 type OSInfo struct {
 	Name    string `json:"os" yaml:"os"`
@@ -10,4 +15,13 @@ type OSInfo struct {
 
 func (oi *OSInfo) String() string {
 	return fmt.Sprintf("%s %s", oi.Name, oi.Version)
+}
+
+// Release returns release version of current running OS. It returns a
+// default string if can't find.
+func Release() string {
+	if oi, err := New(); err != nil {
+		return oi.Version
+	}
+	return UnknownRelease
 }
